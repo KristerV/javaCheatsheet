9144e8a8df4ce0b31327aa3c8d22c64414b92e46
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-05-09 19:17:56 +0300

    advancedMode

--- client/index.js
+++ client/index.js
@@ -1,3 +1,9 @@
+Template.body.helpers({
+	advancedMode: function () {
+		return Session.get('advancedMode')
+	},
+});
+
 Template.body.events({
 	'click h1, click h2, click h3, click h4, click h5, click h6': function(e) {
 		Scroll.click(e)
@@ -6,6 +12,11 @@ Template.body.events({
 		e.preventDefault()
 		var url = $(e.currentTarget).attr('href')
 		Scroll.scrollUrl(url)
+	},
+	'click #advancedButton': function(e) {
+		var advancedMode = !Session.get('advancedMode')
+		Session.set('advancedMode', advancedMode)
+		localStorage.setItem('advancedMode', advancedMode)
 	}
 })
 
@@ -14,6 +25,8 @@ Template.body.rendered = function() {
 }
 
 Meteor.startup(function(){
+	Session.set('advancedMode', JSON.parse(localStorage.getItem('advancedMode')))
+	console.log(JSON.parse(localStorage.getItem('advancedMode')))
 	Meteor.setTimeout(function(){
 		Scroll.scrollUrl()
 	},1000)
