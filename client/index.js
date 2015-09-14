08b80c78d23f894b1c045b6f4583d23011389ba4
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-09-14 20:27:01 +0300

    hotjar fix

--- client/index.js
+++ client/index.js
@@ -39,9 +39,9 @@ Template.body.rendered = function() {
 	Prism.highlightAll()
 
 	// hotjar
-	(function(h,o,t,j,a,r){
+    (function(h,o,t,j,a,r){
         h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-        h._hjSettings={hjid:75422,hjsv:5};
+        h._hjSettings={hjid:75424,hjsv:5};
         a=o.getElementsByTagName('head')[0];
         r=o.createElement('script');r.async=1;
         r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
